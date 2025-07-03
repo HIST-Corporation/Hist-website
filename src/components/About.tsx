d7f391f3f1efc1cab35329cf804248d2e7a9015c
Hist-Corp <hist.corpz@gmail.com>
2025-07-03 22:21:07 +0545

    changed leaders experience , changed links to link

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -30,7 +30,7 @@ const About = () => {
       {
          name: "Om Prakash Bhushal",
          role: "Chief Executive Officer",
-         description: "Leading HIST with 2+ years of experience in IT strategy and business development.",
+         description: "Leading HIST with 5+ years of experience in IT strategy and business development.",
          image: "/CEO.jpg",
          linkedin: "#",
          category: "Leadership",
@@ -38,7 +38,7 @@ const About = () => {
       {
          name: "Kalpana Belbase",
          role: "Chief Financial Officer",
-         description: "Expert in software architecture and emerging technologies with 10+ years experience.",
+         description: "Expert in Finencing in  emerging technologies with 7+ years experience.",
          image: "CFO.jpg",
          linkedin: "#",
          category: "Finance",
