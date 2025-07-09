c1be8db8886ca912fd7dd107116444233c815cd4
Hist-Corp <hist.corpz@gmail.com>
2025-07-09 20:51:30 +0545

    removing arrow in let's get started

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -39,7 +39,7 @@ const Hero = () => {
                         className="w-full sm:w-auto border-2 border-hist-blue text-hist-blue hover:bg-hist-blue hover:text-white px-6 py-4 text-base sm:text-lg font-semibold rounded-lg transition-all duration-300"
                      >
                         Let's Get Started
-                        <ArrowRight className="w-4 h-4 sm:w-5 sm:h-5 ml-2" />
+                       {/* <ArrowRight className="w-4 h-4 sm:w-5 sm:h-5 ml-2" />*/}
                      </Button>
                   </div>
 
